c8d5804bc06713d6fcb6e2c3b89c6ad1106a2c5d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-01 08:46:30 +0300

    Add task ConcatenateData

---
+++ E01.First Step In Coding/T06.ConcatenateData.js
@@ -0,0 +1,9 @@
+function concatanateData(input) {
+    
+    let firstName = input[0];
+    let secondtName = input[1];
+    let age = Number(input[2]);
+    let city = input[3];
+
+    console.log(`You are ${firstName} ${secondtName}, a ${age}-years old person from ${city}.`);
+}
