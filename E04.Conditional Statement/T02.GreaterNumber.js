c3e8c264bcda04fdf4ad1078505ab39f18e3d206
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-09 13:08:26 +0300

    Add task GreaterNumber

---
+++ E04.Conditional Statement/T02.GreaterNumber.js
@@ -0,0 +1,10 @@
+function demo(input) {
+    let firstNum = Number(input[0]);
+    let secondNum = Number(input[1]);
+
+    if (firstNum > secondNum) {
+        console.log(firstNum);
+    }else{
+        console.log(secondNum);
+    }
+}
