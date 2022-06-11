92d452ed98901b12e66f12885d19627ec97c1f9d
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-11 17:30:57 +0300

    Add task WorkingHours

---
+++ E05.Conditional Statement-Exercise/T07.WorkingHours.js
@@ -0,0 +1,12 @@
+function demo(input) {
+    
+    let hour = Number(input[0]);
+    let day = input[1];
+
+    if (hour < 10 || hour > 18 || day === "Sunday") {
+        console.log("closed");
+    }else{
+        console.log("open");
+    }
+
+}
