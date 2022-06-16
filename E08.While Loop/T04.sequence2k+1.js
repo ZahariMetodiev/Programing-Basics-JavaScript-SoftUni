106ba7c2357bd0d50df491b636899798a2240136
ZahariMetodiev <zmetodiev4@gmail.com>
2022-06-16 11:41:20 +0300

    Add task sequence

---
+++ E08.While Loop/T04.sequence2k+1.js
@@ -0,0 +1,11 @@
+function sequence(input) {
+    
+    let number = Number(input[0]);
+    let counter = 1;
+
+    while (counter <= number) {
+        console.log(counter);
+        counter *= 2 + 1;
+    }
+
+}
